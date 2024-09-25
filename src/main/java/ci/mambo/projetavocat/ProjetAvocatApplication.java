8b2d9ee6ad237697d01593adcace8e9b5bed4dc4
Mambo_Christian <mambochristian2018@gmail.com>
2024-09-25 05:37:10 +0000

    first commit

---
+++ src/main/java/ci/mambo/projetavocat/ProjetAvocatApplication.java
@@ -0,0 +1,13 @@
+package ci.mambo.projetavocat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAvocatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetAvocatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/mambo/projetavocat/ProjetAvocatApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.mambo.projetavocat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetAvocatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
